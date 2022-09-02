1ccd90c6a2ddb908a08c63dbf265195f7878fb21
Lee Siang Yin <siangyin916@gmail.com>
2022-09-02 15:56:42 +0800

    add bootstrap lib, components for navbar, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
 	return (
-		<>
-			<h1>hello</h1>
-		</>
+		<div>
+			<Header />
+			<main>
+				<Container>
+					<h1>HI</h1>
+				</Container>
+			</main>
+			<Footer />
+		</div>
 	);
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+const Header = () => {
+	return (
+		<header>
+			<Navbar bg="light" expand="lg" collapseOnSelect>
+				<Container>
+					<Navbar.Brand href="/">LSY-Shop</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ms-auto">
+							<Nav.Link href="/cart">Cart</Nav.Link>
+							<Nav.Link href="/login">Login</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
+
+export default Header;
